f63744c57c3adae0b90259f18b9f1b5f91088aa3
Suzanneshuford <39348006+Suzanneshuford@users.noreply.github.com>
2022-08-05 17:08:11 -0400

    Update streamlit2_app.py

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -59,5 +59,4 @@ if streamlit.button('Get Fruit Load List'):
 # allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
-
-#my_cur.execute("insert into fruit_load_list values('from streamlit')");
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
